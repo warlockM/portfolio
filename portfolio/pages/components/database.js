856a0304a24ab958bc5e156189aeddb6fd83d178
mayank <mnkraj529@gmail.com>
2022-12-18 21:23:17 +0530

    New db added in skills

--- portfolio/pages/components/database.js
+++ portfolio/pages/components/database.js
@@ -2,6 +2,7 @@ export default function Database() {
   return (
     <div class="grid grid-cols-2 justify-items-center">
       <h1 class="text-5xl p-4 bg-slate-200 rounded-md">MySQL</h1>
+      <h1 class="text-5xl p-4 bg-slate-200 rounded-md">Airtable</h1>
       <h1 class="text-5xl p-4 bg-slate-200 rounded-md">Firebase</h1>
     </div>
   );
