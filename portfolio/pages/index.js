b6f8597cd7f1d88bff8abc9facbfcbae48f0b389
mayank <mnkraj529@gmail.com>
2022-12-18 21:32:18 +0530

    link added to connect with me button

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -13,7 +13,10 @@ export default function Home() {
       <div>
       <h1 class="p-2 sm:text-lg md:text-3xl lg:text-5xl font-extrabold">Are you looking for a dynamic professional with Software Engineering and Leadership Experience?</h1>
       <p class="p-2 sm:text-md md:text-xl lg:text-3xl">You are in the right place.</p>
-      <button class="mt-4 ml-2 p-4 bg-blue-700 text-white rounded-md">Hire Now</button></div>
+          <a href="https://calendly.com/mayankraj07/meet-mayank" target="_blank">
+            <button class="mt-4 ml-2 p-4 bg-blue-700 text-white rounded-md">Connect with me</button>
+          </a>
+      </div>
     </div>
     <h1 class="sm:text-3xl md:text-5xl text-center mt-10 font-bold">Experience</h1>
     <Experience />
