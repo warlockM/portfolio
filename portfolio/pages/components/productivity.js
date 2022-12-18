bb98af6e3f9169e2ebf7b95135a1852ebb61f331
mayank <mnkraj529@gmail.com>
2022-12-18 20:08:13 +0530

    More productivity added

--- portfolio/pages/components/productivity.js
+++ portfolio/pages/components/productivity.js
@@ -3,6 +3,9 @@ export default function Productivity() {
     <div class="grid grid-cols-2 justify-items-center">
       <h1 class="text-5xl p-4 bg-slate-200 rounded-md">Confluence</h1>
       <h1 class="text-5xl p-4 bg-slate-200 rounded-md">Product Board</h1>
+      <h1 class="text-5xl p-4 bg-slate-200 rounded-md">JIRA</h1>
+      <h1 class="text-5xl p-4 bg-slate-200 rounded-md">Miro Board</h1>
+      <h1 class="text-5xl p-4 bg-slate-200 rounded-md">Lucid Chart</h1>
     </div>
   );
 }
