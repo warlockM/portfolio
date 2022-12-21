3d1bf7146824b3d47deac50e0cbbf715504e82d6
mayank <mnkraj529@gmail.com>
2022-12-21 23:29:55 +0530

    Fixed download resume page

--- portfolio/pages/components/navbar.js
+++ portfolio/pages/components/navbar.js
@@ -16,7 +16,6 @@ export default function Navbar() {
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             
             <div>
-                <Link href="https://drive.google.com/file/d/1AK3RPyP3tGkvrWNgeAoPbvon5lSKELkV/view?usp=sharing">Download Resume</Link>
                 <Link href="/components/resume"
                 class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-700 hover:bg-white mt-4 lg:mt-0">
                     Download Resume
--- portfolio/pages/components/resume.js
+++ portfolio/pages/components/resume.js
@@ -1,7 +1,9 @@
+import Link from 'next/link'
+
 export default function Resume() {
     return (
-        <div>
-        <h1>Resume</h1>
+        <div class="grid grid-cols-1 justify-items-center">
+        <Link href="https://drive.google.com/file/d/1AK3RPyP3tGkvrWNgeAoPbvon5lSKELkV/view?usp=sharing" target="_blank" class="bg-blue-700 p-4 m-4 text-white font-bold">Download Resume</Link>
         </div>
     )
     }
