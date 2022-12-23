cfeba8c294e8772afc419eff27b7f3e8300f19bd
mayank <mnkraj529@gmail.com>
2022-12-23 23:16:32 +0530

    useref addded

--- portfolio/pages/components/dropComment.js
+++ portfolio/pages/components/dropComment.js
@@ -1,14 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function DropComment(){
+    const [comment, setComment] = useState("")
+    const commentRef = useRef();
+
     function handleSubmit(e) {
         e.preventDefault()
-      console.log("comment dropped")
+        const usercomment = commentRef.current.value
+      console.log(usercomment)
     }
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        <div class="grid grid-cols-1 justify-items-center space-y-2">
         <label>Leave a comment.</label>
-        <input type="text"></input>
-        <button>Submit</button>
+        <input type="text" ref={commentRef}></input>
+        <button class="p-3 bg-blue-700 text-white rounded-md">Submit</button>
+        </div>
       </form>
     </div>
   );
